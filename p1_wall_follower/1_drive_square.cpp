52be88c9c151848597abbc52e312bbe2c6c769cf
SiddhantRBohra <sidrbohra@gmail.com>
2024-09-06 13:42:03 -0400

    first commit

---
+++ p1_wall_follower/1_drive_square.cpp
@@ -0,0 +1,38 @@
+/**
+ * File: drive_square.cpp
+ *
+ * Code to drive in a square N times.
+ */
+
+#include <iostream>
+#include <cmath>
+#include <mbot_bridge/robot.h>
+#include <mbot_lib/utils.h>
+
+
+int main(int argc, const char *argv[])
+{
+    // Initialize the robot.
+    mbot_bridge::MBot robot;
+
+    // Variables to be tuned.
+    float vel = 0.5;
+    float dt = 1.0;
+    int num_square = 3;
+
+    for(int i = 0; i < num_square; ++1){
+        robot.drive(vel, 0.0, 0.0);
+        //probably a sleep duration timer to ensure perfect sequential execution
+        robot.drive(0.0, 0.0, 1.57);
+        for(int j = 0; j < 2; ++j){
+            robot.drive(vel, 0.0, 0.0);
+            //sleep
+            robot.drive(0.0, 0.0, 1.57)
+            //sleep
+        }
+    }
+    
+    std::cout << "Stopping the robot!!" << std::endl;
+    robot.stop();
+    return 0;
+}

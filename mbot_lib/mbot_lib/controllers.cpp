df37c676a060adf99677df022de8e20b9920f219
Farzad Hasan <far.has@outlook.com>
2024-09-18 16:27:21 -0400

    final but not autograder

--- mbot_lib/mbot_lib/controllers.cpp
+++ mbot_lib/mbot_lib/controllers.cpp
@@ -1,25 +1,24 @@
 /**
  * File: controllers.cpp
- *
  * Sources for feedback controllers.
  */
 
 #include <mbot_lib/controllers.h>
 
-
 float bangBangControl(float current, float setpoint, float scaling, float tolerance)
 {
-    if (current > setpoint + tolerance) {
-        return scaling;
-    } else if (current < setpoint - tolerance) {
-        return -scaling;
-    } return 0;
+    if (current > (setpoint + tolerance)) {
+        return -1.0 * scaling; //move forward
+    }
+    if (current < (setpoint - tolerance)) {
+        return 1.0 * scaling; //move backward
+    } else {
+        return 0.0;
+    }
 }
 
 float pControl(float current, float setpoint, float kp)
 {
-    float error = current * setpoint;
+    float error = current - setpoint;
     return kp * error;
-
-    return -0.1;
 }
--- p1_wall_follower/2_follow_1d.cpp
+++ p1_wall_follower/2_follow_1d.cpp
@@ -1,30 +1,21 @@
 /**
  * File: follow_1d.cpp
- *
- * Controls the robot to maintain a given distance to the wall directly in
- * front of it.
- *
- * This code uses Bang-Bang control or P-control to maintain the setpoint
- * distance.
+ * Controls the robot to maintain a given distance to the wall directly in front of it.
+ * This code uses Bang-Bang control or P-control to maintain the setpoint distance.
  */
 
 #include <cmath>
 #include <iostream>
-
 #include <signal.h>
-
 #include <mbot_bridge/robot.h>
-
 #include <mbot_lib/controllers.h>
 #include <mbot_lib/utils.h>
-
 
 bool ctrl_c_pressed;
 void ctrlc(int)
 {
     ctrl_c_pressed = true;
 }
-
 
 int main(int argc, const char *argv[])
 {
@@ -35,9 +26,9 @@ int main(int argc, const char *argv[])
     std::vector<float> ranges;
     std::vector<float> thetas;
 
-    float setpoint = 1.0; 
+    float setpoint = 0.4; 
     float tolerance = 0.1;
-    float scaling = 0.5;
+    float scaling = -0.5;
     float kp = 0.5;
 
     while (true) {
@@ -46,10 +37,12 @@ int main(int argc, const char *argv[])
         float dist_to_wall = findFwdDist(ranges, thetas);
         if (dist_to_wall < 0) continue;
 
-        float controlSignal = bangBangControl(dist_to_wall, setpoint, scaling, tolerance);
-        robot.drive(controlSignal, 0.0, 0.0);
-        //float controlSignal = pControl(dist_to_wall, setpoint, kp);
+        std::cout << "Distance to wall: " << dist_to_wall << std::endl;
 
+        //float controlSignal = pControl(dist_to_wall, setpoint, kp)
+        float controlSignal = bangBangControl(dist_to_wall, setpoint, scaling, tolerance);
+        std::cout << "Control Signal: " << controlSignal << std::endl;  // Debugging output
+        robot.drive(controlSignal, 0.0, 0.0);
 
         if (ctrl_c_pressed)
             break;

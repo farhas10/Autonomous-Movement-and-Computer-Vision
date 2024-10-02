7afac18d7e034a3f5957f20fd40bc9b23e0e835a
Farzad Hasan <48370386+farhas10@users.noreply.github.com>
2024-10-02 14:35:00 -0400

    WallFollower Complete!
    Wall Follower Done

--- p1_wall_follower/5_wall_follower.cpp
+++ p1_wall_follower/5_wall_follower.cpp
@@ -3,76 +3,43 @@
  *
  * Controls robot to follow a wall. 
  */
-
 #include <iostream>
 #include <cmath>
-
 #include <signal.h>
-
 #include <mbot_bridge/robot.h>
-
 #include <mbot_lib/behaviors.h>
 #include <mbot_lib/controllers.h>
 #include <mbot_lib/utils.h>
-
-
 bool ctrl_c_pressed;
 void ctrlc(int)
 {
     ctrl_c_pressed = true;
 }
-
-
 int main(int argc, const char *argv[])
 {
     signal(SIGINT, ctrlc);
     signal(SIGTERM, ctrlc);
-
     // Initialize the robot.
     mbot_bridge::MBot robot;
-
-    // We will store the LiDar scan data in these vectors.
+    // Create empty vectors to store the scan data.
     std::vector<float> ranges;
     std::vector<float> thetas;
 
-    // *** Task 1: Adjust these values appropriately ***
-
-    float setpoint = 0.5;  // The goal distance from the wall in meters
-
-    // *** End student code *** //
+   
     while (true) {
-        // This function gets the Lidar scan data.
+
+        //Reading the surrounding environment into arrays.
         robot.readLidarScan(ranges, thetas);
+         
+         //Computing directional cross product through wall follower command.
+         std::vector<float> vals = computeWallFollowerCommand(ranges, thetas);
 
-        // Get the distance to the wall.
-        float min_idx = findMinNonzeroDist(ranges);
-        float dist_to_wall = ranges[min_idx];
-        float angle_to_wall = thetas[min_idx];
-
-        // *** Task 2: Implement the 2D Follow Me controller ***
-        // Hint: Look at your code from follow_1D
-        // Hint: When you compute the velocity command, you might find the functions
-        // rayConversionVector helpful!
+         //Entering x, y, and angular parameters for robot to drive.
+         robot.drive(vals[0], vals[1], vals[2]);
         
-        float error = setpoint - dist_to_wall; 
-        std::vector<float> directionVector = rayConversionVector(angle_to_wall);
-
-        //Angular and linear constants.
-        float k_linear = 1.0;
-        float k_angular = 2.0;
-
-        //Corresponding velocity constants.
-        float linear_velocity = k_linear * error;
-        float angular_velocity = k_angular * (error/setpoint);
-        
-        std::vector<float> x = pControl(directionVector[0], linear_velocity, error);
-        std::vector<float> y = pControl(directionVector[1], linear_velocity, error);
-        std::vector<float> z = crossProduct(x,y);
-        //Drive in the direction of the vector.
-        robot.drive(x,y,z[2]);
-        
-        //Command stopping the robot.
         if (ctrl_c_pressed) break;
+    }
+    // Stop the robot.
     robot.stop();
     return 0;
 }
